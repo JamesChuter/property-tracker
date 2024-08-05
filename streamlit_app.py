ead5ba043ac174b4d00a6122760f4a5a4db60ba8
JamesChuter <58206404+JamesChuter@users.noreply.github.com>
2024-08-05 01:53:24 +0000

    update heading and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🏡 Property Tracker")
 st.write(
-    "Me start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "All your Property Transactions in one Place"
 )
