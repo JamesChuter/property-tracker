ad4e7bd9985ea27aa17ccda46e745f778779fb54
JamesChuter <58206404+JamesChuter@users.noreply.github.com>
2024-08-05 02:17:06 +0000

    form

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,9 @@ st.write(
 st.header("Add New Property")
 
 with st.form("my form"):
-    st.text_input("Address")
-    st.text_input("Purchase Price")
+    address = st.text_input("Address")
+    purchase_price = st.text_input("Purchase Price")
     st.form_submit_button('Submit')
+
+st.write(address)
+st.write(purchase_price)
