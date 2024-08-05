17d2924cc3691909a30424892ecc4cfe6eadf3ab
JamesChuter <58206404+JamesChuter@users.noreply.github.com>
2024-08-05 02:13:11 +0000

    add first form

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,10 @@ st.title("🏡 Property Tracker")
 st.write(
     "All your Property Transactions in one Place"
 )
+
+st.header("Add New Property")
+
+with st.form("my form")
+    st.text_input("Address")
+    st.text_input("Purchase Price")
+    st.form_submit_button('Submit')
